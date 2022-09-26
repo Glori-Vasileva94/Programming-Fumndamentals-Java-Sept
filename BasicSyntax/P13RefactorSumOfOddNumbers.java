ca6ec9e277c3b52e9d66349d1f92ac0a025a37e6
Glori-Vasileva94 <114347275+Glori-Vasileva94@users.noreply.github.com>
2022-09-26 15:52:59 +0300

    Add files via upload

---
+++ BasicSyntax/P13RefactorSumOfOddNumbers.java
@@ -0,0 +1,21 @@
+package BasicSyntax;
+
+import java.util.Scanner;
+
+public class P13RefactorSumOfOddNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        int n = Integer.parseInt(scanner.nextLine());
+        int sum = 0;
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            result  = 2 * i +1;
+            System.out.println(result);
+            sum += result;
+
+        }
+        System.out.printf("Sum: %d%n", sum);
+    }
+}
